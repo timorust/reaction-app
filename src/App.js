c09c53b9ed85c32b036d738a16e4fcceba7cad8a
Timor Rust <timorust1@gmail.com>
2022-07-18 21:55:07 +0300

    API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react';
 import axios from 'axios'
+import Movie from './Movie';
 
 class App extends React.Component {
 
@@ -10,7 +11,8 @@ class App extends React.Component {
 	}
 
 	getMovies = async () => {
-		const movies = await axios.get('https://yts.mx/api/v2/list_movies.json')
+		const { data: { data: { movies } } } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
+		this.setState({ movies, isLoading: false })
 	}
 
 	componentDidMount() {
@@ -20,9 +22,18 @@ class App extends React.Component {
 
 	render() {
 
-		const { isLoading } = this.state;
+		const { isLoading, movies } = this.state;
 
-		return <div>{isLoading ? 'Loading...' : 'Page loaded'}</div>
+		return <div>{isLoading ? 'Loading...' : movies.map(movie => {
+			console.log(movie);
+			return <Movie
+				key={movie.id}
+				id={movie.id}
+				year={movie.year}
+				title={movie.title}
+				summary={movie.summary}
+				poster={movie.medium_cover_image} />
+		})}</div>
 	}
 }
 
